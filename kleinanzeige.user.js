bebb6bb18e1b0ea49244fad30f069741182f9e3a
Mehmet Ersahin <ersahin.m@eplan.de>
2023-08-31 01:13:52 +0200

    fixed was

--- kleinanzeige.user.js
+++ kleinanzeige.user.js
@@ -1,3 +1,16 @@
+// ==UserScript==
+// @name         kleinanzeige
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       @mersahin
+// @require https://code.jquery.com/jquery-3.6.0.min.js
+// @require file://D:\perso\scripts\kleinanzeige.user.js
+// @match https://www.kleinanzeigen.de/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kleinanzeigen.de
+// @grant        none
+// ==/UserScript==
+
 "use strict";
 // Resimleri göstereceğimiz bir div eklemeliyiz
 $("body").prepend('<div id="yanyana-resimler"></div>');
@@ -7,20 +20,7 @@ const guid_generator = function () {
       .toString(16)
       .substring(1);
   };
-  return (
-    s4() +
-    s4() +
-    "-" +
-    s4() +
-    "-" +
-    s4() +
-    "-" +
-    s4() +
-    "-" +
-    s4() +
-    s4() +
-    s4()
-  );
+  return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
 };
 // Stil eklemek için CSS kullanabilirsiniz
 $("#yanyana-resimler").css({
@@ -30,11 +30,12 @@ $("#yanyana-resimler").css({
 let visitedLinks = [];
 // Zamanlayıcı aralığı (örneğin 5 dakika)
 const interval = 5 * 1000; // 5 saniye
-let url = location.url;// "https://www.kleinanzeigen.de/s-40789/l1116r5";
+let url = location.href;// "https://www.kleinanzeigen.de/s-40789/l1116r5";
 const run = function (url, append = false) {
-  $.get(url, function (data) {
+
+  $.get(`${url}`, function (data) {
     let ilanlar = $(data).find(
-      ".ad-listitem:not(.is-topad) .aditem-main--middle"
+      ".ad-listitem:not(.is-topad) .aditem-main"
     );
     console.log("get", ilanlar.length);
     // if not visited
@@ -44,21 +45,30 @@ const run = function (url, append = false) {
       if (!visitedLinks.includes(link)) {
         let guid = guid_generator();
         // all link one div with guid
-        $("#yanyana-resimler").prepend("<div class='ilan' id='" + guid + "'></div>");
-        // style for .ilan
-        $(".ilan").css({
-          // display: 'inline-flex',
-          border: '2px solid red'
-        });
+        if(append)
+          $("#yanyana-resimler").append("<div class='ilan' id='" + guid + "'></div>");
+        else
+          $("#yanyana-resimler").prepend("<div class='ilan' id='" + guid + "'></div>");
+        // if preise weniger als 10 euro then border is green
+        let fiyat = $(this).find(".aditem-main--middle--price-shipping--price").text().trim()
+        if(fiyat.replace("€", "").replace("VB", "").replace(".", ",") < 10 || fiyat == "Zu verschenken")
+          $(`#${guid}`).css({
+            // display: 'inline-flex',
+            border: '2px solid green',
+            background: 'greenyellow'
+          });
+        else
+          $(`#${guid}`).css({
+            // display: 'inline-flex',
+            border: '2px solid red'
+          });
 
         // add information from class aditem-main--middle
         let aditemMainMiddle = $(this)
         let clonedAditemMainMiddle = aditemMainMiddle.clone();
         styleFixer(clonedAditemMainMiddle);
-        if(append)
-          $(`#${guid}`).append(clonedAditemMainMiddle);
-        else
-          $(`#${guid}`).prepend(clonedAditemMainMiddle);
+
+        $(`#${guid}`).prepend(clonedAditemMainMiddle);
         
         visitedLinks.push(link);
         console.log("new link", link);
@@ -81,14 +91,17 @@ const run = function (url, append = false) {
   });
 };
 // Zamanlayıcıyı başlatma
-run(url);
+run(location.href, true, 1);
 // refresh 5 saniyede bir
-setInterval(run, interval);
+setInterval(function() {
+   run(url)
+}, interval);
 
 // refresh every 20 seconds
 setInterval(function () {
   // location.reload();
 }, 20 * 1000);
+
 function styleFixer(clonedAditemMainMiddle) {
   clonedAditemMainMiddle.css("display", "inline-block");
   clonedAditemMainMiddle.css("border", "3px solid #ccc");
