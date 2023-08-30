4552267191493ffb44da36fc4a66b5bb54f339db
Mehmet Ersahin <ersahin.m@eplan.de>
2023-08-31 01:21:52 +0200

    update url added

--- kleinanzeige.user.js
+++ kleinanzeige.user.js
@@ -6,8 +6,10 @@
 // @author       @mersahin
 // @require https://code.jquery.com/jquery-3.6.0.min.js
 // @require file://D:\perso\scripts\kleinanzeige.user.js
+// @require https://github.com/mersahin/scripts/raw/main/kleinanzeige.user.js
 // @match https://www.kleinanzeigen.de/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kleinanzeigen.de
+// @updateURL https://github.com/mersahin/scripts/raw/main/kleinanzeige.user.js
 // @grant        none
 // ==/UserScript==
 
