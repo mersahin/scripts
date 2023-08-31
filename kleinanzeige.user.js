6baad9308c3329467bc4d4c9b61829a6898c7da1
Mehmet Ersahin <m@ersah.in>
2023-08-31 04:24:12 +0200

    excluded added

--- kleinanzeige.user.js
+++ kleinanzeige.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         kleinanzeige
 // @namespace    http://tampermonkey.net/
-// @version      0.10
+// @version      0.11
 // @description  try to take over the world!
 // @author       @mersahin
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://github.com/mersahin/scripts/raw/main/main.js
 // @match        https://www.kleinanzeigen.de/*
+// @exclude      https://www.kleinanzeigen.de/s-anzeige/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kleinanzeigen.de
 // @updateURL    https://github.com/mersahin/scripts/raw/main/kleinanzeige.user.js
 // @grant        none
