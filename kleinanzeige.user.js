0f37fd1e7a499c7b2875682ee533f0576a6a82fb
mehmet erşahin <m@ersah.in>
2025-05-09 12:53:10 +0200

    Update kleinanzeige.user.js

--- kleinanzeige.user.js
+++ kleinanzeige.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         kleinanzeige2
 // @namespace    http://tampermonkey.net/
-// @version      0.33
+// @version      0.34
 // @description  try to take over the world!
 // @author       @mersahin
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      ./main.js
 // @match        https://www.kleinanzeigen.de/*
 // @exclude      https://www.kleinanzeigen.de/s-anzeige/*
+// @exclude      https://www.kleinanzeigen.de/m-nachrichten*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kleinanzeigen.de
 // @updateURL    https://github.com/mersahin/scripts/raw/main/kleinanzeige.user.js
 // @downloadURL  https://github.com/mersahin/scripts/raw/main/kleinanzeige.user.js
